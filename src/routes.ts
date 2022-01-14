0f307e3c32568191ea755bab10e79eeacdfe20e8
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-01-13 22:32:26 -0400

    feat: create routes file

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({
+    name: "NodeJs",
+    duration: 10,
+    educator: "Dani"
+  });
+
+  return response.send();
+}
